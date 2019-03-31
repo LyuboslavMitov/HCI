112858961c08e92c7c10bca8a6bac056819feddb
Veronika Galinova <vgvalentinova@abv.bg>
2019-03-31 22:22:50 +0300

    implement function for adding new line

---
+++ scripts/lines.js
@@ -0,0 +1,29 @@
+var allLines = [];
+
+function addLine() {
+    let startPoint = document.getElementById('startPoint').value;
+    let endPoint = document.getElementById('endPoint').value;
+    let stops = document.getElementById('stops').value;
+    let duration = document.getElementById('duration').value;
+    let price = document.getElementById('price').value;
+    let distance = document.getElementById('distance').value;
+
+    let dataForLine = [startPoint, endPoint, stops, duration, price, distance];
+    allLines.push(dataForLine);
+
+    var tableRef = document.getElementById('lines').getElementsByTagName('tbody')[0];
+
+    var newRow = tableRef.insertRow();
+
+    for (let i = 0; i < dataForLine.length; i++) {
+        var newCell = newRow.insertCell(i);
+        var newText = document.createTextNode(dataForLine[i]);
+        newCell.appendChild(newText);
+    }
+
+    var removeButton = newRow.insertCell();
+    removeButton.innerHTML = `<button>Remove line <i class="fa fa-trash" aria-hidden="true"></i></button>`;
+    
+
+    event.preventDefault();
+}
