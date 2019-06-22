9f8645810de17f8bfeb9ac7ed05d6bf27567d8a6
Lyuboslav Mitov <lmitov@uni-sofia.bg>
2019-06-22 16:48:59 +0300

    Added menu component

--- tbb-client/src/app/app.module.ts
+++ tbb-client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyLinesComponent } from './components/company-lines/company-lines.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyLinesComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
