53f76fe4f19b60d26f2953dbc1538d2a4e8f68d5
Lyuboslav Mitov <lmitov@uni-sofia.bg>
2019-06-24 19:20:13 +0300

    Added tickets model

---
+++ tbb-client/src/app/models/ticket.ts
@@ -0,0 +1,10 @@
+export interface Ticket {
+    id:number;
+    startPoint:string;
+    endPoint:string;
+    stops:string[];
+    duration:string;
+    price:number;
+    distance:string;
+    company:string;
+}
