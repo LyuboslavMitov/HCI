c79e7c7328793b24ba63ea728e133cb430412bb6
Lyuboslav Mitov <lmitov@uni-sofia.bg>
2019-06-24 21:48:49 +0300

    Added search-routes

--- tbb-client/src/app/components/search-route/search-route.component.ts
+++ tbb-client/src/app/components/search-route/search-route.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BusLine } from 'src/app/models/BusLine';
+import { CompanyService } from 'src/app/services/company.service';
 
 @Component({
   selector: 'app-search-route',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-route.component.css']
 })
 export class SearchRouteComponent implements OnInit {
+  companyLines: BusLine[];
 
-  constructor() { }
+  cities: string[] = ["Sofia",
+    "St Zagora",
+    "Plovdiv",
+    "Varna",
+    "Vidin",
+    "Ruse",
+    "Bourgas",
+    "Pazardzhik",]
+
+  defaultOption = 'Sofia';
+
+  sortOptions: string[] = ["Duration", "Price", "Distance"]
+  startPoint: string = this.defaultOption;
+  endPoint: string = this.defaultOption;
+  sortBy: string;
+
+  buttonText: string = "Buy ticket";
+
+  constructor(private service: CompanyService) { }
 
   ngOnInit() {
+    this.getLines();
   }
+  buyTicket(event: any) {
 
+  }
+  changeStartingPoint(event: any) {
+    this.startPoint = event.value;
+  }
+  changeEndPoint(event: any) {
+    this.endPoint = event.value;
+  }
+  changeSortOption(event: any) {
+    this.sortBy = event.value;
+  }
+  private getLines() {
+    this.service.getLines().subscribe(res => {
+      this.companyLines = res;
+      console.log(this.companyLines)
+    });
+  }
+  filter() {
+    this.getLines();
+    this.companyLines = this.companyLines.filter(l => l.startPoint === this.startPoint && l.endPoint === this.endPoint);
+      switch(this.sortBy) {
+        case "Duration": this.companyLines.sort((a,b) => a.duration-b.duration);break;
+        case "Price": this.companyLines.sort((a,b)=>a.price-b.price);break;
+        case "Distance": this.companyLines.sort((a,b)=>a.distance-b.distance);break;
+        default:break;
+      }
+  }
 }
